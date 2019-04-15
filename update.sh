f767bbfcbcf80f7970028687281a41b37753a5d1
Alex Nanu <35782655+jigodia@users.noreply.github.com>
2019-04-16 00:07:18 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -62,8 +62,8 @@ git pull origin master
 chmod +x update.sh
 get_const_global="src/consts/const_global.js"
 set_cputhreads
-cd dist_bundle/argon2-cpu-miner/ && rm -rf ../CPU && mkdir ../CPU && mkdir build && cd build && cmake -DCMAKE_BUILD_TYPE=Release .. && make && cp libargon2.so libargon2.so.1 ../../../ && cp cpu-miner ../../CPU/ &&
-2-cpu-miner && npm install
+cd dist_bundle/argon2-cpu-miner/ && rm -rf ../CPU && mkdir ../CPU && mkdir build && cd build && cmake -DCMAKE_BUILD_TYPE=Release .. && make && cp libargon2.so libargon2.so.1 ../../../ && cp cpu-miner ..
+undle/argon2-cpu-miner && npm install
 
 echo -e
 echo -e "$showinfo BACM pool miner installed."
